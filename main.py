cb3f8e477d05324025d2cec81aa9de9f2adb6ca8
Ali Kadhim <ali@scicoded.com>
2021-03-21 15:40:36 -0500

    Modified spacing in main

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ for i in range(50):
     random_younger = randint(13,64)
     train_samples.append(random_younger)
     train_labels.append(1)
-    
+
     # The ~5% of older individuals who did not experience side effects
     random_older = randint(65,100)
     train_samples.append(random_older)
@@ -33,18 +33,18 @@ for i in range(1000):
     random_younger = randint(13,64)
     train_samples.append(random_younger)
     train_labels.append(0)
-    
+
     # The ~95% of older individuals who did experience side effects
     random_older = randint(65,100)
     train_samples.append(random_older)
     train_labels.append(1)
-    
+
 # for i in train_samples:
 #     print(i)
 #
 # for i in train_labels:
 #     print(i)
-    
+
 
 #%% shuffle order and normalize data
 
@@ -61,7 +61,7 @@ scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
 
 # for i in scaled_train_samples:
 #     print(i)
-    
+
 #%% Simple tf.keras Sequential Model
 
 import tensorflow as tf
@@ -103,7 +103,7 @@ import os
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-                                                      
+
 model.fit(
     x=scaled_train_samples,
     y=train_labels,
@@ -129,7 +129,7 @@ for i in range(10):
     random_younger = randint(13,64)
     test_samples.append(random_younger)
     test_labels.append(1)
-    
+
     # The 5% of older individuals who did not experience side effects
     random_older = randint(65,100)
     test_samples.append(random_older)
@@ -140,12 +140,12 @@ for i in range(200):
     random_younger = randint(13,64)
     test_samples.append(random_younger)
     test_labels.append(0)
-    
+
     # The 95% of older individuals who did experience side effects
     random_older = randint(65,100)
     test_samples.append(random_older)
     test_labels.append(1)
-    
+
 test_labels = np.array(test_labels)
 test_samples = np.array(test_samples)
 test_labels, test_samples = shuffle(test_labels, test_samples)
@@ -154,7 +154,7 @@ scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1,1))
 
 #%% Predict
 
-predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=0) 
+predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=0)
 
 rounded_predictions = np.argmax(predictions, axis=-1)
 
@@ -176,24 +176,24 @@ def plot_confusion_matrix(cm,
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting 'normalize=True'.
     """
-    
+
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
-    
+
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print("Confusion matrix without normalization")
-    
+
     print(cm)
-    
+
     thresh = cm.max()/2.
-    
+
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, cm[i, j],
                  horizontalalignment="center",
@@ -201,10 +201,10 @@ def plot_confusion_matrix(cm,
         plt.tight_layout()
         plt.ylabel('True label')
         plt.xlabel('Predicted label')
-        
+
 cm_plot_labels = ['no_side_effects', 'had_side_effects']
 plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title = 'Confusion Matrix')
-                 
+
 
 #%% Save and Load a Model
 
@@ -225,13 +225,13 @@ json_string = model.to_json()
 #     json.dump(json_string[0], outfile)
 
 
-#%% JSON to Model    
+#%% JSON to Model
 # model reconstruction from JSON:
 from tensorflow.keras.models import model_from_json
 
 # with open('json_model.json','r') as json_file:
 #     json_string_new = json.load(json_file)
-    
+
 json_model = model_from_json(json_string)
 json_model.summary()
 
@@ -241,36 +241,15 @@ json_model.summary()
 import os.path
 if os.path.isfile('models/ali_model_weights.h5') is False:
     model.save_weights('models/ali_model_weights.h5')
-    
+
 model2 = Sequential([
     Dense(units=16, input_shape=(1,), activation='relu'),
     Dense(units=32, activation='relu'),
     Dense(units=2, activation='softmax')
 ])
-    
-    
+
+
 model2.load_weights('models/ali_model_weights.h5')
 model2.get_weights()
 
 model2.get_config()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
